8eb950d21be5882ec98e0975623112f740686f0a
Sandy Raveloarisoa <sandy.rav@onja.org>
2020-09-10 15:02:20 +0300

    started working on the image slider to show the big version each of them whe it's cliked

--- webroot/index.js
+++ webroot/index.js
@@ -1,11 +1,25 @@
 console.log('it works');
-const nextArrow = document.querySelector('.arrow-next');
-console.log(nextArrow);
-let index = 0;
+// grab all necessary elements
+const bigImage = document.querySelector('.big_image');
+const images = document.querySelectorAll('.image_container');
+const largeImgSrc = bigImage.querySelector('.big_src');
+const mdImgSrc = bigImage.querySelector('.md_src');
+const smImgSrc = bigImage.querySelector('.sm_src');
+console.log(bigImage);
+let currentImg;
 
-function nextImage() {
-    index += 1;
-    if (index > images.length - 1) {
-        index = 0;
-    }
+// show image
+function showImage(element) {
+    largeImgSrc.srcset = element.children[0].srcset;
+    mdImgSrc.srcset = element.children[1].srcset;
+    smImgSrc.src = element.children[2].src;
+    currentImg = element;
 }
+
+// loop through each image to add event listener to show each image in the big container
+images.forEach(image => {
+    image.addEventListener('click', e => {
+        let img = e.target.parentElement;
+        showImage(img);
+    })
+})
